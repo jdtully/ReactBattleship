d0a5deb4ccf668799f9cf6a5fdf96d5ffa0a952f
Jeff <jefftully@gmail.com>
2019-07-16 13:11:52 -0400

    testing

--- Board.jsx
+++ Board.jsx
@@ -4,6 +4,7 @@ import Square from "./Square";
 class Board extends React.Component {
   render() {
     console.log(this.props);
+    console.log("je je");
     const status = "Next player: X";
     const rows = [...Array(this.props.rows).keys()];
     const cols = [...Array(this.props.cols).keys()];
