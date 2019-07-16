5152c631b32873f24d11b514ddcdfcb4b62ca201
jdtully <48932074+jdtully@users.noreply.github.com>
2019-07-16 12:52:20 -0400

    Add files via upload

---
+++ Board.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Square from "./Square";
+
+class Board extends React.Component {
+  render() {
+    console.log(this.props);
+    const status = "Next player: X";
+    const rows = [...Array(this.props.rows).keys()];
+    const cols = [...Array(this.props.cols).keys()];
+
+    var result = (
+      <div>
+        <div className="status">{status}</div>
+        {rows.map(y => (
+          <div>
+            {cols.map(x => (
+              <Square data-x={x} data-y={y} />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+
+    return result;
+  }
+}
+
+export default Board;
